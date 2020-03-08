8bc05d5f28689359f5e6e75f9b29f1b25922c3b3
losuffi <losuffi@hotmail.com>
2020-03-08 18:53:49 +0800

    使用Cubemap的预计算阴影方式的验证

--- Scripts/UShadowCubemapBaker.cs
+++ Scripts/UShadowCubemapBaker.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class UShadowBakerConfig
 {
-    [Range(0,5)]
-    public int Precision = 2;
+    // [Range(0,5)]
+    // public int Precision = 2;
 }
 [ExecuteInEditMode]
 public class UShadowCubemapBaker : MonoBehaviour
@@ -16,8 +16,11 @@ public class UShadowCubemapBaker : MonoBehaviour
     private Shader GetDepthShader;
 
     private Camera DepthCamera;
+    // [UnityEngine.SerializeField]
+    // private RenderTexture DepthRT;
+
     [UnityEngine.SerializeField]
-    private RenderTexture DepthRT;
+    private Cubemap CubemapTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,15 +45,17 @@ public class UShadowCubemapBaker : MonoBehaviour
         DepthCamera.transform.position = Pos;
         DepthCamera.clearFlags = CameraClearFlags.SolidColor;
         DepthCamera.backgroundColor = Color.white;
+        DepthCamera.fieldOfView = 45;
+        DepthCamera.farClipPlane = 50;
         GetDepthShader = Shader.Find("Hidden/RenderDepth");
         if(GetDepthShader == null)
         {
             Debug.LogError("Dont find 'RenderDepth' shader! please keep project is complete!");
         }
-        DepthRT = new RenderTexture(32 << Config.Precision, 32 << Config.Precision, 0, RenderTextureFormat.R16);
-        DepthRT.enableRandomWrite = true;
-        DepthRT.filterMode = FilterMode.Bilinear;
-        DepthRT.Create();
+        // DepthRT = new RenderTexture(32 << Config.Precision, 32 << Config.Precision, 0, RenderTextureFormat.R16);
+        // DepthRT.enableRandomWrite = true;
+        // DepthRT.filterMode = FilterMode.Bilinear;
+        // DepthRT.Create();
     }
     [ContextMenu("Run")]
     public void BakeRender()
@@ -68,20 +73,53 @@ public class UShadowCubemapBaker : MonoBehaviour
         {
             return;
         }
-        if(DepthRT == null)
+        // if(DepthRT == null)
+        // {
+        //     return;
+        // }
+        if(CubemapTarget == null)
         {
+            Debug.LogError("Target Cubemap Is Null please set cubemap target");
             return;
         }
         DepthCamera.transform.rotation = new Quaternion(0,0,0,1);
-        DepthCamera.targetTexture = DepthRT;
+        // DepthCamera.targetTexture = DepthRT;
         DepthCamera.SetReplacementShader(GetDepthShader, "");
-        DepthCamera.Render();
+        DepthCamera.RenderToCubemap(CubemapTarget);
+        CubemapTarget.Apply();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.PositiveZ);
+        // DepthCamera.transform.rotation = new Quaternion(0, Mathf.Sin(0.25f * Mathf.PI), 0, Mathf.Cos(0.25f * Mathf.PI));
+        // DepthCamera.Render();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.PositiveX);
+        // DepthCamera.transform.rotation = new Quaternion(0, Mathf.Sin(0.5f * Mathf.PI), 0, Mathf.Cos(0.5f * Mathf.PI));
+        // DepthCamera.Render();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.NegativeZ);
+        // DepthCamera.transform.rotation = new Quaternion(0, Mathf.Sin(0.75f * Mathf.PI), 0, Mathf.Cos(0.75f * Mathf.PI));
+        // DepthCamera.Render();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.NegativeX);
+        // DepthCamera.transform.rotation = new Quaternion(Mathf.Sin(0.25f * Mathf.PI), 0, 0, Mathf.Cos(0.25f * Mathf.PI));
+        // DepthCamera.Render();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.PositiveY);
+        // DepthCamera.transform.rotation = new Quaternion(Mathf.Sin(0.75f * Mathf.PI), 0, 0, Mathf.Cos(0.75f * Mathf.PI));
+        // DepthCamera.Render();
+        // WriteToCubemap(DepthRT, CubemapTarget, CubemapFace.NegativeY);
+    }
+
+    private void WriteToCubemap(RenderTexture renderTexture, Cubemap Target, CubemapFace face)
+    {
+        RenderTexture.active = renderTexture;
+        Texture2D FaceTexture2D = new Texture2D(renderTexture.width,renderTexture.height, TextureFormat.ARGB32, 0, true);
+        FaceTexture2D.ReadPixels(new Rect(0,0,renderTexture.width, renderTexture.height), 0, 0);
+        FaceTexture2D.Apply();
+        Target.SetPixels(FaceTexture2D.GetPixels(), face);
+        Target.Apply();
+        RenderTexture.active = null;
     }
     [ContextMenu("Reset")]
     public void ResetData()
     {
         DepthCamera = null;
-        DepthRT = null;
+        // DepthRT = null;
         GetDepthShader = null;
     }
 }
